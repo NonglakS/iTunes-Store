3fc9ff59a3892294a7c089ab1acee5bb17fbc817
Nonglak Saeueng <mam.nonglaks@gmail.com>
2021-08-22 23:10:24 -0700

    change layout

--- music-hub/src/Components/Album.js
+++ music-hub/src/Components/Album.js
@@ -1,11 +1,10 @@
 function Album() {
   return (
     <div className="Album container">
-      <img alt="album-img" src="https://is3-ssl.mzstatic.com/image/thumb/Music124/v4/79/53/35/79533511-13b3-62c3-941d-bf87e21940f8/source/100x100bb.jpg" />
+      <img id="album-img" alt="album-img" src="https://is3-ssl.mzstatic.com/image/thumb/Music124/v4/79/53/35/79533511-13b3-62c3-941d-bf87e21940f8/source/100x100bb.jpg" />
       <div className="album-info container">
-        <div>Album Name: Let the Sun Goes Down</div>
-        <div>Year Released: 2021</div>
-        <div>Number of tracks: 13</div>
+        <div className="album-name">Let the Sun Goes Down</div>
+        <div>2021</div>
       </div>
     </div>
   );
--- music-hub/src/Components/Artist.js
+++ music-hub/src/Components/Artist.js
@@ -3,6 +3,7 @@ function Artist() {
   return (
     <div className="Artist container">
       <header className="Artist-header">
+        <img id="artist-img" alt="artist-img" src="https://is3-ssl.mzstatic.com/image/thumb/Music124/v4/79/53/35/79533511-13b3-62c3-941d-bf87e21940f8/source/100x100bb.jpg"/>
         <h1>Artist Name</h1>
       </header>
 
